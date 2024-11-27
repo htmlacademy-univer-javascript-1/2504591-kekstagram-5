705f0229f9565174cafe7aa82e46f2107f560023
Keks <keks@htmlacademy.ru>
2024-11-27 18:37:34 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/main.js
@@ -0,0 +1,8 @@
+import {renderSimularPictures} from './render.js';
+import { openForm } from './form.js';
+import { scaleImg } from './scale.js';
+import { createSlider } from './slider.js';
+renderSimularPictures();
+openForm();
+scaleImg();
+createSlider();
