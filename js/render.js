aa3c6b930f9342d7c7963a85c1772a3c4989989d
Evgeni <qazplm2125a@gmail.com>
2024-12-11 17:02:53 +0500

    12.12

--- js/render.js
+++ js/render.js
@@ -1,17 +1,47 @@
 import { renderBigPicture } from './big-picture.js';
-import { onfilterClick } from './filters.js';
 import { debounce } from './util.js';
+
+const ACTIVE_CLASS = 'img-filters__button--active';
+
+const filterRandom = document.querySelector('#filter-random');
+const filterDiscussed = document.querySelector('#filter-discussed');
+const filterDefault = document.querySelector('#filter-default');
 
 const pictureSection = document.querySelector('.pictures');
 const templatePic = document.querySelector('#picture').content.querySelector('.picture');
 let pictureListFragment = document.createDocumentFragment();
 let defaultPictures = '';
 
+function shuffleArray(array) {
+  const length = array.length;
+  const shuffle = array.slice();
+  for (let i = length - 1; i > 0; i -= 1) {
+    const random = Math.floor(Math.random() * (i + 1));
+    const current = shuffle[i];
+    shuffle[i] = shuffle[random];
+    shuffle[random] = current;
+  }
+  return shuffle.slice(0, 10);
+}
+
+function sortByCommentCount(a, b) {
+  return b.comments.length - a.comments.length;
+}
+
+function filterPictures(pictureArray) {
+  if (filterRandom.classList.contains(ACTIVE_CLASS)) {
+    return shuffleArray(pictureArray);
+  } else if (filterDiscussed.classList.contains(ACTIVE_CLASS)) {
+    return pictureArray.slice().sort(sortByCommentCount);
+  }
+  return pictureArray;
+}
+
 function updatePictureList(picturesList) {
   if (defaultPictures === '') {
     defaultPictures = picturesList;
   } else {
-    pictureSection.querySelectorAll('.picture').forEach((el) => (el.remove()));
+    pictureSection.querySelectorAll('.picture').forEach((el) => el.remove());
     pictureListFragment = document.createDocumentFragment();
   }
 }
@@ -38,14 +68,23 @@ const renderPictures = function(picturesList) {
   document.querySelector('.img-filters').classList.remove('img-filters--inactive');
 };
 
-const filterRandom = document.querySelector('#filter-random');
-const filterDiscussed = document.querySelector('#filter-discussed');
-const filterDefault = document.querySelector('#filter-default');
 
-const debouncedOnfilterClick = debounce(onfilterClick);
+const onFilterClickDebounced = debounce((filterButton) => {
+  document.querySelector('.img-filters__button--active').classList.remove(ACTIVE_CLASS);
+  filterButton.classList.add(ACTIVE_CLASS);
 
-debouncedOnfilterClick(filterDefault);
-debouncedOnfilterClick(filterDiscussed);
-debouncedOnfilterClick(filterRandom);
+  if (defaultPictures !== '') {
+    renderPictures(filterPictures(defaultPictures));
+  }
+});
 
-export {renderPictures,defaultPictures};
+function addFilterClickListener(filterButton) {
+  filterButton.addEventListener('click', () => onFilterClickDebounced(filterButton));
+}
+
+
+addFilterClickListener(filterDefault);
+addFilterClickListener(filterDiscussed);
+addFilterClickListener(filterRandom);
+
+export { renderPictures, defaultPictures };
